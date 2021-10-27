8baead091768871fadb68e17c09470adc5f6e5a3
Camila Fonseca <camilafonseca.ang7@gmail.com>
2021-10-27 00:53:23 -0300

    feat: return of the last three messages functionality implemented

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
+import { GetLast3MessagesController } from './controllers/GetLast3MessagesController'
 
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
@@ -9,5 +10,6 @@ const router = Router()
 
 router.post('/authenticate', new AuthenticateUserController().handle)
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
+router.get('/messages/last3', new GetLast3MessagesController().handle)
 
 export { router }
